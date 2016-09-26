02021e1ed6c394087d752a1859b921154228e8ad
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2016-09-26 17:22:57 -0300

    chore(tests): Integrate Cucumber with Capybara
    Also, removes the rspec/capybara integration.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # To avoid random errors about missing constants when running capybara
+  # ref: https://github.com/jnicklas/capybara#using-capybara-with-cucumber
+  config.allow_concurrency = false
 end
